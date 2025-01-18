f28675513610a5badc8158661a3aecb53af02ecb
Azam <swooshfx@gmail.com>
2025-01-18 10:48:12 +0800

    Write and read new Firebase data.

--- app/src/main/java/com/traxtivemotor/ReadJSONFromAssets.kt
+++ app/src/main/java/com/traxtivemotor/ReadJSONFromAssets.kt
@@ -33,9 +33,23 @@ data class Profile(
     val userName: String? = null
 )
 
+data class Profile2(
+    val dateOfBirth: String? = null,
+    val email: String? = null,
+    val fullName: String? = null,
+    val gender: String? = null,
+    val mobileNumber: String? = null,
+    val userName: String? = null
+)
+
 data class ServiceUpdate(
     val date: String? = null,
     val motorId: String? = null,
+    val nextMileage: String? = null,
+)
+
+data class ServiceUpdate2(
+    val date: String? = null,
     val nextMileage: String? = null,
 )
 
@@ -120,10 +134,10 @@ fun parseJson(context: Context, path: String) {
 //                    println("- motorcycle: $motorcycle")
 //
 //                    val motor = Motorcycle(motorcycle.brand, motorcycle.imageUrl, motorcycle.model, motorcycle.plateNumber)
-//                    database.reference.child("motorcycles-v2").child(userId).child(key).setValue(motor)
+//                    database.reference.child("motorcycles").child(userId).child(key).setValue(motor)
 //                }
 ////                addMotor(userId)
-//                return@lit
+////                return@lit
 //            }
 
 //            motorcyclesData.userProfile.forEach { (userId, userMap) ->
@@ -133,9 +147,10 @@ fun parseJson(context: Context, path: String) {
 //                    println("- user ID 2: $userId2")
 //                    println("- profile: $profile")
 //
-////                    database.reference.child("userProfile-v2").child(userId).setValue(profile) // remove userId
+//                    val profile2 = Profile2(profile.dateOfBirth, profile.email, profile.fullName, profile.gender, profile.mobileNumber, profile.userName)
+//                    database.reference.child("userProfile").child(userId).setValue(profile2) // remove userId
 //                }
-//                return@lit
+////                return@lit
 //            }
 
 //            motorcyclesData.serviceUpdate.forEach { (userId, motorMap) ->
@@ -148,10 +163,11 @@ fun parseJson(context: Context, path: String) {
 //                        println("  - motor: $motor")
 //                        println("  - serviceUpdate: $serviceUpdate")
 //
-////                        database.reference.child("serviceUpdate-v2").child(userId).child(motorId).setValue(serviceUpdate) // remove motorId
+//                        val update = ServiceUpdate2(serviceUpdate.date, serviceUpdate.nextMileage)
+//                        database.reference.child("serviceUpdate").child(userId).child(motorId).setValue(update)
 //                    }
 //                }
-//                return@lit
+////                return@lit
 //            }
 
             motorcyclesData.services.forEach { (userId, motorcycleMap) ->
@@ -168,7 +184,7 @@ fun parseJson(context: Context, path: String) {
                         println("  - price05: ${service.price05?.isBlank()}")
 
                         val service2 = Service2(service.date, service.mileage, service.price, service.remark, service.workshop, service.year)
-                        database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).setValue(service2)
+                        database.reference.child("services").child(userId).child(motorId).child(serviceId).setValue(service2)
 
                         val serviceList = mutableListOf<Item>()
                         if (service.service01?.isNotBlank() == true) {
@@ -176,76 +192,76 @@ fun parseJson(context: Context, path: String) {
                             println(item01)
                             serviceList.add(item01)
 
-                            database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item01)
+                            database.reference.child("services").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item01)
                         }
                         if (service.service02?.isNotBlank() == true) {
                             val item02 = Item(service.service02, service.price02!!)
                             println(item02)
                             serviceList.add(item02)
 
-                            database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item02)
+                            database.reference.child("services").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item02)
                         }
                         if (service.service03?.isNotBlank() == true) {
                             val item03 = Item(service.service03, service.price03!!)
                             println(item03)
                             serviceList.add(item03)
 
-                            database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item03)
+                            database.reference.child("services").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item03)
                         }
                         if (service.service04?.isNotBlank() == true) {
                             val item04 = Item(service.service04, service.price04!!)
                             println(item04)
                             serviceList.add(item04)
 
-                            database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item04)
+                            database.reference.child("services").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item04)
                         }
                         if (service.service05?.isNotBlank() == true) {
                             val item05 = Item(service.service05, service.price05!!)
                             println(item05)
                             serviceList.add(item05)
 
-                            database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item05)
+                            database.reference.child("services").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item05)
                         }
                         if (service.service06?.isNotBlank() == true) {
                             val item06 = Item(service.service06, service.price06!!)
                             println(item06)
                             serviceList.add(item06)
 
-                            database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item06)
+                            database.reference.child("services").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item06)
                         }
                         if (service.service07?.isNotBlank() == true) {
                             val item07 = Item(service.service07, service.price07!!)
                             println(item07)
                             serviceList.add(item07)
 
-                            database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item07)
+                            database.reference.child("services").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item07)
                         }
                         if (service.service08?.isNotBlank() == true) {
                             val item08 = Item(service.service08, service.price08!!)
                             println(item08)
                             serviceList.add(item08)
 
-                            database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item08)
+                            database.reference.child("services").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item08)
                         }
                         if (service.service09?.isNotBlank() == true) {
                             val item09 = Item(service.service09, service.price09!!)
                             println(item09)
                             serviceList.add(item09)
 
-                            database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item09)
+                            database.reference.child("services").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item09)
                         }
                         if (service.service10?.isNotBlank() == true) {
                             val item10 = Item(service.service10, service.price10!!)
                             println(item10)
                             serviceList.add(item10)
 
-                            database.reference.child("services-v2").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item10)
+                            database.reference.child("services").child(userId).child(motorId).child(serviceId).child("items").push().setValue(item10)
                         }
                         println(serviceList)
                     }
                 }
                 println("\n\n\n")
-                return@lit
+//                return@lit
             }
         }
 //        println("totalCount: $totalCount")
